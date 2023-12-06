de185d6000f01e9c63280c26eda2991f0a34fbb3
Tohirjon-Odilov <shahanshoh819@gmail.com>
2023-12-06 22:14:00 +0500

    add calculator

--- 1_lesson_Hello_World/1_lesson_Hello_World/Program.cs
+++ 1_lesson_Hello_World/1_lesson_Hello_World/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Salom, Dunyo");
+Console.WriteLine("Calculator!");
+
+Console.WriteLine("Enter the number: ");
+Console.Write("Enter the a = ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter ishora: ");
+char hint = Convert.ToChar(Console.Read());
+
+switch(hint) 
+{
+    case '+':
+        Console.WriteLine(a+b);
+        break;
+    case '-':
+        Console.WriteLine(a - b);
+        break;
+    case '*':
+        Console.WriteLine(a * b);
+        break;
+    case '/':
+        Console.WriteLine(a / b);
+        break;
+    default: 
+        Console.WriteLine("Error");
+        break;
