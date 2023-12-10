33107b6ac7245338616d627346eee7ad6c07b149
Tohirjon-Odilov <shahanshoh819@gmail.com>
2023-12-10 18:33:28 +0500

    add some files

--- 1_month_C#/3_lesson_operator/Program.cs
+++ 1_month_C#/3_lesson_operator/Program.cs
@@ -24,14 +24,14 @@
 // left shift 9 >> 2, 9 / (2*2) ya'ni 2 ni darajasiga ko'paytirib 9 bo'lish orqali taqriban hisob lashimiz mumkin
 // 9 / (2*2) = 9 / 4 = 2
 
-int a = 1;
-a++;
-a--;
+//int a = 1;
+//a++;
+//a--;
 
-int numberOne = 9;
-float numberTwo = 4;
+//int numberOne = 9;
+//float numberTwo = 4;
 //int result = numberOne / numberTwo;
-float result2 = numberOne / numberTwo;
+//float result2 = numberOne / numberTwo;
 
 //Console.WriteLine(nameof(result2));
 //Console.WriteLine(sizeof(int));
@@ -39,12 +39,101 @@ float result2 = numberOne / numberTwo;
 // dotnet'ga 
 //Console.WriteLine(typeof(string));
 
-string color = "red";
-string result = color switch
-{
-    "red" => "The color is red.",
-    "green" => "The color is green",
-    _ => "Unknown color"
-};
+//string color = "red";
+//string result = color switch
+//{
+//    "red" => "The color is red.",
+//    "green" => "The color is green",
+//    _ => "Unknown color"
+//};
 
-Console.WriteLine(result);
+//Console.WriteLine(result);
+
+
+/////////////// Vazifa
+//Boolean 34
+/*Console.Write("x = ");
+int x = int.Parse(Console.ReadLine());
+Console.Write("y = ");
+int y = int.Parse(Console.ReadLine());
+
+if ((x + y) % 2 == 0)
+{
+    Console.WriteLine("Qora");
+}
+else
+{
+    Console.WriteLine("Oq");
+}
+
+Console.ReadKey();*/
+
+
+// Boolean 35
+
+/*Console.Write("x1 = ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y1 = ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("x2 = ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y2 = ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+
+if ((x1 + y1) % 2 == 0 && (x2 + y2) % 2 == 0 || (x1 + y1) % 2 == 0 && (x2 + y2) % 2 == 0)
+{
+    Console.WriteLine(true);
+}
+else
+{
+    Console.WriteLine(false);
+}
+*/
+
+/*Console.Write("x1 = ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y1 = ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("x2 = ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y2 = ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+
+if  (x1 == x2 || y1 == y2)
+{
+    Console.WriteLine(true);
+}
+else
+{
+    Console.WriteLine(false);
+}*/
+
+
+// Boolean 37
+
+/*Console.Write("x1 = ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y1 = ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("x2 = ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y2 = ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+int res = x1 + y1;
+int res2 = x2 + y2;
+
+if (Math.Abs(res - res2) == 1)
+{
+    Console.WriteLine(true);
+}
+else
+{
+    Console.WriteLine(false);
+}*/
+
