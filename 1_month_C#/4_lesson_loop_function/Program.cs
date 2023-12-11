3984d1b45c705a3270344187c43de025338ae7d8
Tohirjon-Odilov <shahanshoh819@gmail.com>
2023-12-11 12:57:23 +0500

    update files

--- 1_month_C#/4_lesson_loop_function/Program.cs
+++ 1_month_C#/4_lesson_loop_function/Program.cs
@@ -1,11 +1,14 @@
-﻿//void Sum(decimal a, decimal b)
+﻿// Funksiyadan foydalandik //
+//void Sum(decimal a, decimal b)
 //{
 //    Console.WriteLine(a + b);
 //}
 
 //Sum(15, 12.5m);
 
-decimal Sum(params decimal[] list)
+
+// params 'dan foydalandik //
+/*decimal Sum(params decimal[] list)
 {
     decimal sum = 0;
     for (int i = 0; i < list.Length; i++)
@@ -16,3 +19,12 @@ decimal Sum(params decimal[] list)
 }
 
 Console.WriteLine(Sum(45.5454m, 4545, 454545, 4545.859m));
+*/
+
+decimal i = 0;
+decimal sum(decimal a)
+{
+    return sum(i+1);
+}
+
+Console.WriteLine(sum(5));
