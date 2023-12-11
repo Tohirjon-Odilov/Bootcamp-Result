d7d76f137fe08f9881e370253000c6e6ceaf1108
Tohirjon-Odilov <shahanshoh819@gmail.com>
2023-12-11 10:27:53 +0500

    add first function

--- 1_month_C#/4_lesson_loop_function/Program.cs
+++ 1_month_C#/4_lesson_loop_function/Program.cs
@@ -1,6 +1,18 @@
-﻿void Sum(decimal a, decimal b)
+﻿//void Sum(decimal a, decimal b)
+//{
+//    Console.WriteLine(a + b);
+//}
+
+//Sum(15, 12.5m);
+
+decimal Sum(params decimal[] list)
 {
-    Console.WriteLine(a + b);
+    decimal sum = 0;
+    for (int i = 0; i < list.Length; i++)
+    {
+        sum += list[i];
+    }
+    return sum;
 }
 
-Sum(15, 12.5m);
+Console.WriteLine(Sum(45.5454m, 4545, 454545, 4545.859m));
