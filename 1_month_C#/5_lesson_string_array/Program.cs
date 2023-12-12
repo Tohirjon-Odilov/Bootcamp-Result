f9782ea08e4f863535833fedd8350b994199e7bb
Tohirjon-Odilov <shahanshoh819@gmail.com>
2023-12-12 10:26:42 +0500

    add lesson 5 string

---
+++ 1_month_C#/5_lesson_string_array/Program.cs
@@ -0,0 +1,14 @@
+﻿string item = "bu yerda nimadur kelyapti \"nimadur bomi\" nima gap";
+Console.WriteLine(item);
+
+
+
+
+
+//string []item1 = ["salom", "hello"];
+
+//foreach(string i in item1)
+//{
+//    Console.WriteLine(i);
+//}
+//Console.WriteLine(item1);
