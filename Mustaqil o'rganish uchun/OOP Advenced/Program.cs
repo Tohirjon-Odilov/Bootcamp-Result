93ba82ee9c186e09270f2a5b51847d426aeea3d8
Tohirjon-Odilov <shahanshoh819@gmail.com>
2023-12-18 21:44:52 +0500

    Add static class and update files

---
+++ Mustaqil o'rganish uchun/OOP Advenced/Program.cs
@@ -0,0 +1,4 @@
+﻿// static class'dan object olib bo'lmaydi.
+//var static_test = new static_clash(); // xato ishlaydi
+
+Console.WriteLine("Hello world");
---
+++ Mustaqil o'rganish uchun/OOP Advenced/static_clash.cs
@@ -0,0 +1,24 @@
+﻿namespace OOP_Advenced
+{
+    public static class static_clash
+    {
+
+        // static class'da constructor bo'lmaydi.
+        //public static_clash() {}
+        public static void MyMessages()
+        {
+            Console.WriteLine("Bu static class");
+        }
+
+    }
+
+    public class NormalClass
+    {
+        //static class'dan voris olib bo'lmaydi
+        public static void MyClass()
+        {
+            GC.Collect();
+            Console.WriteLine("Static method");
+        }
+    }
+}
