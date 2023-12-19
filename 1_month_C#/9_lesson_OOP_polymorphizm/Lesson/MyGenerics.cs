62d94f01ee0d214e8aa32c41c2f8c0e8b7dde6be
Tohirjon-Odilov <shahanshoh819@gmail.com>
2023-12-19 19:53:04 +0500

    add generics files and update

--- 1_month_C#/9_lesson_OOP_polymorphizm/Lesson/MyGenerics.cs
+++ 1_month_C#/9_lesson_OOP_polymorphizm/Lesson/MyGenerics.cs
@@ -1,6 +1,14 @@
-﻿//namespace _9_lesson_OOP_polymorphizm
-//{
-//    internal class MyGenerics
-//    {
-//    }
-//}
+﻿namespace _9_lesson_OOP_polymorphizm
+{
+    internal class MyGenerics <MyString, MyInt>
+    {
+        public MyString? name;
+        public MyInt? year;
+        public MyInt? month;
+        public MyInt? day;
+        public MyGenerics()
+        {
+            Console.WriteLine($"Maning ismim {name}. Men {year} - yil {month} - oy {day} da tug'ilganman. ");
+        }
+    }
+}
