a49d105e90162db5c189538a67e47a8ffe5f7108
Tohirjon-Odilov <shahanshoh819@gmail.com>
2023-12-19 19:48:20 +0500

    add enum value

--- 1_month_C#/9_lesson_OOP_polymorphizm/Lesson/MyEnum.cs
+++ 1_month_C#/9_lesson_OOP_polymorphizm/Lesson/MyEnum.cs
@@ -1,7 +1,16 @@
-﻿//namespace _9_lesson_OOP_polymorphizm
-//{
-//    internal class MyEnum<T, G>
-//    {
-
-//    }
-//}
+﻿namespace _9_lesson_OOP_polymorphizm
+{
+    internal enum MyEnum
+    {
+        primitive = 101,
+        affirmative = 202,
+        postive = 404,
+        negative = 303,
+        negative_affirmative = 505,
+        negative_negative = 606,
+        negative_negative_affirmative,
+        position,
+        reference = 1001,
+        dynamic
+    }
+}
