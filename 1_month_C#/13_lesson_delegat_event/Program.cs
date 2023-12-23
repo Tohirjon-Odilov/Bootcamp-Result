ea0f350def33c45e55665d911e9bb349edd5af48
Tohirjon <coderr819@gmail.com>
2023-12-23 09:31:12 +0500

    update files

--- 1_month_C#/13_lesson_delegat_event/Program.cs
+++ 1_month_C#/13_lesson_delegat_event/Program.cs
@@ -1,6 +1,3 @@
 ﻿int? a = null;
 int? b = 34;
-
-int? z = a == null ? b : b == null ? a : a == null && b == null ? null : a;
-
-Console.WriteLine(z);
+Console.WriteLine(a == null ? b : b == null ? a : a == null && b == null ? null : a);
