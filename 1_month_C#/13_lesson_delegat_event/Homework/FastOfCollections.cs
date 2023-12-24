04c877e49ee6f56a669273a0164f3bac8a7ee0ea
Tohirjon <coderr819@gmail.com>
2023-12-24 23:49:37 +0500

    Collection'lar tezligi o'rtasidagi farqlar

---
+++ 1_month_C#/13_lesson_delegat_event/Homework/FastOfCollections.cs
@@ -0,0 +1,49 @@
+﻿/*
+Array:
+oxiriga qo'shish: O(n)
+oxiridan olib tashlash: O(n)
+o'rtasiga ma'lumot qo'shish: O(n)
+o'rtadan olib tashlash: O(n)
+Muayyan elementni qidirish: O(n)
+Izoh: Xotiradan samarali foydalanadi; ma'lumotlar hajmi aniqlangan hollarda foydalanamiz.
+
+List:
+oxiriga qo'shing: best case O(1); worst case O(n)
+oxiridan olib tashlash: O(1)
+o'rtasiga ma'lumot qo'shish: O(n)
+o'rtadan olib tashlash: O(n)
+Muayyan elementni qidirish: O(n)
+Izoh: Ko'p hollarda List eng yaxshi tanlov bo'ladi.
+
+LinkedList:
+oxiriga qo'shing: O(1)
+oxiridan olib tashlash: O(1)
+o'rtasiga ma'lumot qo'shish: O(1)
+o'rtadan olib tashlash: O(1)
+Muayyan elementni qidirish: O(n)
+Izoh: Ushbu collection juda ham tez ishlaydi ammo xotiradan yutqazib qo'yamiz.
+
+Stack:
+oxiriga qo'shing: best case O(1); worst case O(n)
+oxiridan olib tashlash: O(1)
+o'rtasiga ma'lumot qo'shish: N/A
+o'rtadan olib tashlash: N/A
+Muayyan elementni qidirish: N/A
+Izoh: uning asosiy xususiyati o'ngdan chiqarilish (LIFO - Last In, First Out) prinsipi bo'yicha ishlashidir.
+
+Queue:
+oxiriga qo'shing: best case O(1); worst case O(n)
+oxiridan olib tashlash: O(1)
+o'rtasiga ma'lumot qo'shish: N/A
+o'rtadan olib tashlash: N/A
+Muayyan elementni qidirish: N/A
+Izoh:  uning asosiy xususiyati chapdan o'qish (FIFO - First In, First Out) prinsipi bo'yicha ishlashidir.
+
+Dictionary:
+oxiriga qo'shing: best case O(1); worst case O(n)
+oxiridan olib tashlash: O(1)
+o'rtasiga ma'lumot qo'shish: best case O(1); worst case O(n)
+o'rtadan olib tashlash: O(1)
+Muayyan elementni qidirish: O(1)
+Izoh:  kalit (key) va qiymat (value) orqali juftlikdagi ma'lumotlarni saqlash uchun ishlatiladi. Bu to'plam turi, kalitlarni o'z ichiga qabul qilib, ularga tezkor murojaat qilish imkonini beradi..
+*/
--- 1_month_C#/13_lesson_delegat_event/Program.cs
+++ 1_month_C#/13_lesson_delegat_event/Program.cs
@@ -2,3 +2,16 @@
 //int? b = 34;
 //Console.WriteLine(a == null ? b : b == null ? a : a == null && b == null ? null : a);
 
+#region s
+//collections
+//Array =>
+//Ma'lumotni oxiriga qo'shish uchun har bir index'dan o'tib qo'shadi. 
+//Ma'lumotni Oxirida olish uchun ham har bir element'ni o'qib keyin oxiridan oladi.
+
+//List<T> =>
+//Collection<T> => 
+//LinkedList<T> =>
+//Stack<T> =>
+//Queue<T> =>
+//Dictionary<TKey, TValue> =>
+#endregion 
