7582654fd64fa278e35faad4db8a2829eb83a892
Tohirjon <coderr819@gmail.com>
2023-12-26 08:25:41 +0500

    update some files

--- 1_month_C#/14_lesson_Func_Action_Predicate_Lyambda_Anonym_function/Lesson/MyEvent.cs
+++ 1_month_C#/14_lesson_Func_Action_Predicate_Lyambda_Anonym_function/Lesson/MyEvent.cs
@@ -1,28 +1,28 @@
-﻿namespace _14_lesson_Func_Action_Predicate_Lyambda_Anonym_function.Lesson
-{
-    public class MyEvent
-    {
-        // delegat'lar Action, Func, Predicate
-        public static void letsgo() 
-        {
-            method1();
-            method2();
-            method3();
-        }
+﻿//namespace _14_lesson_Func_Action_Predicate_Lyambda_Anonym_function.Lesson
+//{
+//    public class MyEvent
+//    {
+//        // delegat'lar Action, Func, Predicate
+//        public static void letsgo() 
+//        {
+//            method1();
+//            method2();
+//            method3();
+//        }
 
-        public static void method1()
-        {
-            Console.WriteLine("Method1 ishladi");
-        }
+//        public static void method1()
+//        {
+//            Console.WriteLine("Method1 ishladi");
+//        }
 
-        public static void method2() 
-        {
-            Console.WriteLine("Method2 ishladi");
-        }
-        public static void method3() 
-        {
-            Console.WriteLine("Method3 ishladi");
-        }
+//        public static void method2() 
+//        {
+//            Console.WriteLine("Method2 ishladi");
+//        }
+//        public static void method3() 
+//        {
+//            Console.WriteLine("Method3 ishladi");
+//        }
 
-    }
-}
+//    }
+//}
