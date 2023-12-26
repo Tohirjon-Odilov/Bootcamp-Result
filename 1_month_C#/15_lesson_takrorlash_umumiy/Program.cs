a5b0b4879da7b2c03f997ffbb04ea2606ac23257
Tohirjon <shahanshoh819@gmail.com>
2023-12-26 18:21:39 +0500

    update files

--- 1_month_C#/15_lesson_takrorlash_umumiy/Program.cs
+++ 1_month_C#/15_lesson_takrorlash_umumiy/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿//bool? isColorValid = false;
+//Console.WriteLine(isColorValid.Value);
+
+//dynamic myXmlDocument = false;
+//Console.WriteLine(myXmlDocument.GetType());
+
+var myCustomer = new
+{
+    Name = "Abdulla",
+    Age = 40
+};
+Console.WriteLine(myCustomer.Name.GetType());
