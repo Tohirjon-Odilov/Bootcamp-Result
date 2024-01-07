790c1f6f81fedb5d95089a6a246d031a595fd827
Tohirjon <shahanshoh819@gmail.com>
2024-01-07 22:36:14 +0500

    update files

---
+++ 1_month_C#/19_lesson_exam_1_1/MyFunc.cs
@@ -0,0 +1,19 @@
+﻿namespace _19_lesson_exam_1_1
+{
+    public class MyFunc
+    {
+        public MyFunc()
+        {
+            Func<int, int, string> myFuncAdd = Add;
+            Console.WriteLine(myFuncAdd(2,9));
+        }
+
+        // oldin e'lon qilib olsa ham bo'ladi.
+        //public delegate TResult Func<in T1, in T2, out TResult>(T1 arg, T2 arg2);
+
+        private static string Add(int arg1, int arg2)
+        {
+            return Convert.ToString(arg1 + arg2) + " Chiqdimi";
+        }
+    }
+}
---
+++ 1_month_C#/19_lesson_exam_1_1/delegat.cs
@@ -0,0 +1,7 @@
+﻿namespace _19_lesson_exam_1_1
+{
+    public class delegat
+    {
+        
+    }
+}
