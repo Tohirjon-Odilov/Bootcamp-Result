cf4e11975e1633bfde0562f9d83a19c6fd91861d
tohirjon-odilov <shahanshoh819@gmail.com>
2024-01-11 15:54:51 +0500

    fixed some bugs

--- 2_month_SQL_Telegram_Bot/22_lesson_http_client/Homework/Movies/Omdbapi.Server.cs
+++ 2_month_SQL_Telegram_Bot/22_lesson_http_client/Homework/Movies/Omdbapi.Server.cs
@@ -2,6 +2,7 @@
 {
     public class Server
     {
+        public ResponseData AllData { get; set; }
         public Server()
         {
             //Console.Write("Enter film name: ");
@@ -11,12 +12,57 @@
             //    Course("Spiderman", 1).Wait();
 
             //Result / await / Wait() ma'lumot yechadi
-            var data = Omdbapi.GetAllData("Spiderman", 1).Result;
-            //Console.WriteLine(data.Response);
-            foreach (var item in data.Search)
+
+            AllData = Omdbapi.GetAllData("Spiderman", 1).Result;
+            //var data1 = Omdbapi.GetAllData("Spiderman", 2).Result;
+
+            int choose = 0;
+            while (true)
             {
-                Console.WriteLine(item.Title);
+                for (int i = 0; i < AllData.Search.Count; i++)
+                {
+                    if (choose == i)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(AllData.Search[choose].Title);
+                        Console.ResetColor();
+                        if (choose == AllData.Search.Count - 1)
+                        {
+                            choose = -1;
+                        }
+                        //if (choose == -1)
+                        //{
+                        //    choose = AllData.Search.Count - i;
+                        //}
+                        //Console.WriteLine(i);
+                    }
+                    else
+                    {
+                        Console.WriteLine(AllData.Search[i].Title);
+                    }
+
+                }
+                Console.WriteLine(choose);
+                choose = UserSelected(Console.ReadKey().Key,choose);
             }
+        }
+        public int UserSelected(ConsoleKey key, int choose)
+        {
+            Console.Clear();
+            string ConvertedKey = Convert.ToString(key)!;
+            if (ConvertedKey == "UpArrow")
+            {   if (choose <= -1)
+                {
+                    choose = AllData.Search.Count-1;
+                }
+                return --choose;
+            }
+            else if (ConvertedKey == "DownArrow") return ++choose;
+            //else if(ConvertedKey == "Enter")
+            //{
+
+            //}
+            else { return choose; }
         }
     }
 }
