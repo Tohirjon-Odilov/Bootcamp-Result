3fed600288264d3fbc1edb20d0fe387eb63d9a99
tohirjon-odilov <shahanshoh819@gmail.com>
2024-01-13 11:44:58 +0500

    add rapid api

--- 2_month_SQL_Telegram_Bot/24_lesson_Takrorlash_Telegram_bot/Program.cs
+++ 2_month_SQL_Telegram_Bot/24_lesson_Takrorlash_Telegram_bot/Program.cs
@@ -1,4 +1,6 @@
 ﻿using _24_lesson_Takrorlash_Telegram_bot;
 
-FirstTelegramBot firstMessage =  new FirstTelegramBot();
-firstMessage.FirstMessage().Wait();
+//FirstTelegramBot firstMessage =  new FirstTelegramBot();
+//firstMessage.FirstMessage().Wait();
+RapidApi rapidApi = new RapidApi();
+rapidApi.Api().Wait();
