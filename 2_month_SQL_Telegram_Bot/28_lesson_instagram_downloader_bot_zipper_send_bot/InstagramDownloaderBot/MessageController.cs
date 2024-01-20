5fc684b95082788f452a48aa1d2f56f0d1796fa8
Akramjon009 <akramzonabduvahobov6@gmail.com>
2024-01-20 17:14:08 +0500

    fixed some bugs

--- 2_month_SQL_Telegram_Bot/28_lesson_instagram_downloader_bot_zipper_send_bot/InstagramDownloaderBot/MessageController.cs
+++ 2_month_SQL_Telegram_Bot/28_lesson_instagram_downloader_bot_zipper_send_bot/InstagramDownloaderBot/MessageController.cs
@@ -15,7 +15,7 @@ namespace _28_lesson_instagram_downloader_bot_zipper_send_bot
             var handler = update.Message.Type switch
             {
                 MessageType.Text => TextAsyncFunction(botClient, update, cancellationToken),
-                MessageType.Document => DocumentAsyncFunction(botClient, update, cancellationToken),
+                _ => OtherMessage(botClient, update, cancellationToken),
             };
         }
 
@@ -33,6 +33,13 @@ namespace _28_lesson_instagram_downloader_bot_zipper_send_bot
             var chatId = update.Message.Chat.Id;
 
             Console.WriteLine($"Received a '{message.Text}' message in chat {chatId}. UserName =>  {message.Chat.Username}");
+            //await botClient.SendVideoAsync(
+            //           chatId: chatId,
+            //           video: "https://smvd-videos-downloader.ugo-code-studio.workers.dev/download?t=aHR0cHM6Ly9zY29udGVudC5jZG5pbnN0YWdyYW0uY29tL3YvdDY2LjMwMTAwLTE2LzQzOTQ4MjEwXzE0MDE3MDM0NDc0NDI1NzdfNzMxNjcyMzk0OTUyMTgxMjkwNV9uLm1wND9lZmc9ZTMwJl9uY19odD1zY29udGVudC5jZG5pbnN0YWdyYW0uY29tJl9uY19jYXQ9MTA5Jl9uY19vaGM9VWNsMVVnRkVpNlFBWDg0Ti1hQiZlZG09QVBzMTdDVUJBQUFBJmNjYj03LTUmb2g9MDBfQWZDR1ZTQjlKS0lQYVViY1BsQTE2c3R6S3pWMGRMVTFzNl8zQkNBZmZqSGh3dyZvZT02NUIxNkNBOCZfbmNfc2lkPTEwZDEzYiZkbD0x&filename=text+video",
+            //           replyToMessageId: message.MessageId,
+            //           //supportsStreaming: true,
+            //           cancellationToken: cancellationToken);
+            //return;
             if (messageText.StartsWith("https://www.instagram.com"))
                 try
                 {
@@ -59,6 +66,7 @@ namespace _28_lesson_instagram_downloader_bot_zipper_send_bot
                             await botClient.SendVideoAsync(
                                chatId: chatId,
                                video: $"{item.url}",
+                               replyToMessageId: message.MessageId,
                                supportsStreaming: true,
                                cancellationToken: cancellationToken);
                         }
@@ -71,6 +79,7 @@ namespace _28_lesson_instagram_downloader_bot_zipper_send_bot
                             );
                             await botClient.SendPhotoAsync(
                                chatId: chatId,
+                               replyToMessageId: message.MessageId,
                                photo: $"{item.url}",
                                cancellationToken: cancellationToken);
                         }
@@ -79,29 +88,70 @@ namespace _28_lesson_instagram_downloader_bot_zipper_send_bot
                 catch (Exception ex)
                 {
                     Console.WriteLine($"{ex.Message}");
+                    await botClient.SendChatActionAsync(
+                        chatId: message.Chat.Id,
+                        chatAction: ChatAction.Typing,
+                        cancellationToken: cancellationToken
+                    );
+                    await botClient.SendTextMessageAsync(
+                        chatId: message.Chat.Id,
+                        text: ex.Message,
+                        replyToMessageId: message.MessageId,
+                        cancellationToken: cancellationToken
+                    );
                     string replasemessage = messageText.Replace("www.", "dd");
                     await botClient.SendVideoAsync
                     (
                            chatId: chatId,
                            video: $"{replasemessage}",
+                           replyToMessageId: message.MessageId,
                            supportsStreaming: true,
                            cancellationToken: cancellationToken
                     );
                 }
+            else if (messageText.StartsWith("https://"))
+            {
+                await botClient.SendChatActionAsync(
+                    chatId: message.Chat.Id,
+                    chatAction: ChatAction.Typing,
+                    cancellationToken: cancellationToken
+                );
+                await botClient.SendTextMessageAsync(
+                    chatId: message.Chat.Id,
+                    text: "Iltimos instagram link kiriting!",
+                    replyToMessageId: message.MessageId,
+                    cancellationToken: cancellationToken
+                );
+            }
             else
             {
-                Console.WriteLine("Foydalanuvchi boshqa narsa jo'natdi!");
+                await botClient.SendChatActionAsync(
+                     chatId: message.Chat.Id,
+                     chatAction: ChatAction.Typing,
+                     cancellationToken: cancellationToken
+                 );
+                await botClient.SendTextMessageAsync(
+                    chatId: message.Chat.Id,
+                    text: "Siz boshqa narsa kiritishga harakat qilyapsiz bloklanasiz!",
+                    replyToMessageId: message.MessageId,
+                    cancellationToken: cancellationToken
+                );
             }
-        }
-
-        private async Task DocumentAsyncFunction(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
         }
 
         public async Task OtherMessage(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            await botClient.SendChatActionAsync(
+                     chatId: update.Message.Chat.Id,
+                     chatAction: ChatAction.Typing,
+                     cancellationToken: cancellationToken
+                 );
+            await botClient.SendTextMessageAsync(
+                chatId: update.Message.Chat.Id,
+                text: "Siz boshqa narsa kiritib bug 🐞 chiqaradigan \nkimsalardek harakat qilyapsiz! 😡 \nIltimos instagram link tashlang!!!",
+                replyToMessageId: update.Message.MessageId,
+                cancellationToken: cancellationToken
+            );
         }
     }
 }
