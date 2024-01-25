71edc357b1e77e839a55bca4e9770253834ba4da
Tohirjon-Odilov <shahanshoh819@gmail.com>
2024-01-25 21:18:31 +0500

    add homework Vazifa. a.Equals(b) -> true bo'ladigan qiling ya'ni ismlari bir xil bo'lgan Person lar teng.

---
+++ 2_month_SQL_Telegram_Bot/33_lesson_takrorlash_experience/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace _33_lesson_takrorlash_experience
+{
+    public class Person
+    {
+        string Name { get; set; }
+        public Person(string name)
+        {
+            Name = name;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            Person otherPerson = (Person)obj;
+            return Name == otherPerson.Name;
+        }
+    }
+}
--- 2_month_SQL_Telegram_Bot/33_lesson_takrorlash_experience/Program.cs
+++ 2_month_SQL_Telegram_Bot/33_lesson_takrorlash_experience/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _33_lesson_takrorlash_experience;
+
+// shu qatordan keyingi codelar main functionni ichida 
+var a = new Person("John");
+var b = new Person("John");
+Console.WriteLine(a.Equals(b));
+
+
+// natija: False (chunki default reference bilan tekshiradi)
+
+// Vazifa. a.Equals(b) -> true bo'ladigan qiling ya'ni ismlari bir xil bo'lgan Person lar teng.
