a96148ca1c6499ad4ca91c4f3317689085694d9c
Tohirjon-Odilov <shahanshoh819@gmail.com>
2024-02-02 08:45:19 +0500

    add files

---
+++ 2_month_SQL_Telegram_Bot/39_lesson_exam_1_kun_telegram_bt/Program.cs
@@ -0,0 +1,129 @@
+﻿//1.Quiz - Bot
+
+//Online test ishlash bot yasang.
+//	Bunda datur ishga tushganda bitta admin va test ishlash uchun baza shakllangan holatda ishga kamida ikkita fandan test ishlash bazasi shakllangan holatda ishga tushsin ( fanlar va ular bo'yicha har biri uchun testlar va testlarga variant javoblar qo’shilgan holarda. Bunda har bir fan uchun alohida togri javob uchun belgilangan ball ham kiritilgan bolsin).
+//Registration ---> Bot ishga tushganda foydalanuvchilar dan share kontact qilishni so’rasin va 	share qilingan raqamga tasdiqlash kodi jo’natilsin. Bu uchun Twilio API dan foydalanish 	tavsiya qilinadi.
+//Admin ---> Agar Tasdiqlash kodini to’g’ri kiritgan foydalanuvchi admin bo’lsa, unga 	quyidagi imkoniyatlar berilsin : 
+//	1.Foydalanuvchilar ro’yxatini excel file ko’rinishida yuklab olish
+//	2.Barcha fanlar bo’yicha ishlangan testlarni pdf file ko’rinishida yuklab olish
+//	3.Fan CRUD
+//	4.Test va javoblar CRUD
+//User ---> Agar Tasdiqlash kodini to’g’ri kiritgan foydalanuvchi oddiy 
+//         foydalanuvchi bo’lsa, unga quyidagi imkoniyatlar berilsin :
+//	1.Fan tanlash va tanlagan fani bo’yicha testlar ishlashg
+//	2.Barcha oldin ishlagan testlarini statistikasini pdf file ko’rinishida yuklab olish.
+
+//_____________________________________________________________________________________________________________________________________
+
+//2.Online-Market-1
+
+//Online telefon sotadigan bot yasash.
+//	Bunda dastur ishga tushganda bitta admin, user bor va sotish uchun telefonlar bazasi shakllangan holatda  ishga tushsin.
+//	Registration ---> Bot ishga tushganda foydalanuvchilar dan share kontact qilishni so’rasin va share qilingan raqamga tasdiqlash kodi jo’natilsin. Bu uchun Twilio API dan foydalanish tavsiya qilinadi.
+//Admin ---> Agar Tasdiqlash kodini to’g’ri kiritgan foydalanuvchi admin bo’lsa, unga quyidagi imkoniyatlar berilsin : 
+//	1.Telefon CRUD qilish
+//	2.Telefonlar sotuvi statistikasini excel file ko’rinishida yuklab olish
+//	3.Olib ketish office larini CRUD qilish : 
+//	4.Barcha mijozlar ro’yxatini viloyatlar kesimida  pdf file ko’rinishida yuklab olish
+//	5.Admin OrderStatus CRUD qilish.(NEW , DELIVERED, ...) 
+//	6.Istalgan buyurtmani statusini o’zgartirish
+//	7.PayType CRUD qilish. ( NAQD , PAYME, CLICK,... )
+//	8.Barcha tolov turlari bo’yicha to’lovlar tarxini excel filega yuklab olish 
+//User ---> Agar Tasdiqlash kodini to’g’ri kiritgan foydalanuvchi oddiy foydalanuvchi bo’lsa, unga quyidagi imkoniyatlar berilsin : 
+//        1.Telefonlarni korish va savatchaga qoshish. Savatchaga qoshishdan oldin nechta dona ekanligini kiritsin
+//        2.Savatcha ko’shilgan mahsulotlarini ro’yxatini ko’rish va istasa shu ro’yxatdagilarni buyurtma qilish yoki ro’yxatdan biror mahsulotni ochirish yoki savatni butunlay tozlash.
+//        3.Barcha qilgan buyurtmalarini pdf file ko’rinishida yuklab olish 
+//        4.Savatdagi yig’ilgan mahsulotlarni buyurtma qilmoqchi bo’lsa buyurtmani dostavka qilinishi yoki olib ketish officelaridan biridan olib ketishni tanlasin va to’lov turini tanlasin va haridni yakunlasin. Bunda to’lovni mahsulot yetkazilganda yoki officelardan biridan olib ketayotganda amalga oshish mumkin agar naqd bo’lsa , agar payme click kabi lar orqali bo’lsa tolov turini tanlash bilan tolandi hisoblanaversin
+
+
+//_____________________________________________________________________________________________________________________________________
+
+
+//3.Online-Market-2
+
+//FastFood bot yasash. 
+//	Bunda dastur ishga tushganda bitta admin user bor va sotish uchun fastfood menusi bazasi shakllangan holatda  ishga tushsin. 
+//Registration ---> Bot ishga tushganda foydalanuvchilar dan share kontact qilishni so’rasin va share qilingan raqamga tasdiqlash kodi jo’natilsin. Bu uchun Twilio API dan foydalanish tavsiya qilinadi.
+//Admin ---> Agar Tasdiqlash kodini to’g’ri kiritgan foydalanuvchi admin bo’lsa, unga quyidagi imkoniyatlar berilsin :
+//	1.Category CRUD(Ichimliklar, Burgerlar, Lavashlar...)
+
+//    2.Product CRUD(sotuvga qoyilgan mahsulotlar CocaCola, Cofe, Choy, Bigburger, Chizburger... )
+
+//    3.PayType  CRUD(NAQD, CLICK, PAYME …. )
+
+//    4.OrderStatus  CRUD(NEW, COMPLETED, DELIVERED... )
+
+//    5.Buyurtmalarni statuslarini o’zgartirish
+//	6.Barcha buyurtmalarni ro’yxatini excel file ko’rinishida yuklab olish
+//	7.Mijozlar ro’yxatini pdf file ko'rinishida yuklab olish 
+//User ---> Agar Tasdiqlash kodini to’g’ri kiritgan foydalanuvchi oddiy foydalanuvchi bo’lsa, unga quyidagi imkoniyatlar berilsin : 
+//        1.Mahsulotlarni korish va savatchaga qoshish. Savatchaga qoshishdan oldin nechta dona ekanligini kiritsin
+//        2.Savatcha ko’shilgan mahsulotlarini ro’yxatini ko’rish va istasa shu ro’yxatdagilarni buyurtma qilish yoki ro’yxatdan biror mahsulotni ochirish yoki savatni butunlay tozlash.
+//        3.Barcha qilgan buyurtmalarini pdf file ko’rinishida yuklab olish 
+//        4.Savatdagi yig’ilgan mahsulotlarni buyurtma qilmoqchi bo’lsa buyurtmani dostavka qilinishi uchun location jo’natsin  va to’lov turini tanlasin va haridni yakunlasin. Bunda to’lovni mahsulot yetkazilganda amalga oshish mumkin agar naqd bo’lsa , agar payme click kabi lar orqali bo’lsa tolov turini tanlash bilan tolandi hisoblanaversin
+
+//_____________________________________________________________________________________________________________________________________
+
+//4.Online-Market-3
+
+//Book (Kitob) bot yasash.
+//	Bunda dastur ishga tushganda bitta admin user bor va sotish uchun audioBook bazasi shakllangan holatda ishga tushsin.
+//Registration ---> Bot ishga tushganda foydalanuvchilar dan share kontact qilishni so’rasin va share qilingan raqamga tasdiqlash kodi jo’natilsin. Bu uchun Twilio API dan foydalanish tavsiya qilinadi.
+
+//Admin ---> Agar Tasdiqlash kodini to’g’ri kiritgan foydalanuvchi admin bo’lsa, unga quyidagi imkoniyatlar berilsin :
+//	1.Category CRUD(Jahon Adabiyoti, O’zbek Adabiyoti, Bolalar Adabiyoti, AudioBook, E - Book, PrintedBook)
+
+//    2.Book CRUD
+//	3.PayType  CRUD ( NAQD, CLICK, PAYME …. )
+//	4.OrderStatus  CRUD  ( NEW , COMPLETED, DELIVERED ... )
+//	5.Buyurtmalarni statuslarini o’zgartirish
+//	6.Barcha buyurtmalarni ro’yxatini excel file ko’rinishida yuklab olish
+//	7.Mijozlar ro’yxatini pdf file ko'rinishida yuklab olish 
+//User ---> Agar Tasdiqlash kodini to’g’ri kiritgan foydalanuvchi oddiy foydalanuvchi bo’lsa, unga quyidagi imkoniyatlar berilsin : 
+//        1.Tanlangan categoriya boyicha  book sotib olish jarayonida to’lov turini tanlagan holda to’lov qilish.
+//	2.To’lov amalga oshishi bilan unga sotib olgan kitobi agar audio yoki e-book bo’lsa yuklab olishi uchun tashab beriladi aks holda yetkazib berish uchun location tashash so’raladi va to’lov mahsulot dostavka qilingandan so’ng (naqd bolsa)  olinadi. aks holda To’lov turini tanlashi bilan shartli ravishda to’landi qilib qoyamiz
+//        3.Bacha sotib olgan agar  audio va e-book kitoblarini qayta yuklab olishi mumkin.
+//        4.Barcha Haridlarini pdf file ko’rinishida yuklab olishi mumkin 
+
+//_____________________________________________________________________________________________________________________________________
+
+//5.Downloader Bot
+
+//VKM Botga o'xshash bot yasalsin
+//	Bunda dastur ishga tushganda bitta admin user bor va sotish uchun musiqalar bazasi yoki musiqa tashlovchi api shakllangan holatda ishga tushsin.
+//Registration ---> Bot ishga tushganda foydalanuvchilar dan share kontact qilishni so’rasin va share qilingan raqamga tasdiqlash kodi jo’natilsin. Bu uchun Twilio API dan foydalanish tavsiya qilinadi.
+
+//Admin ---> 
+//	1.Birdaniga hammaga bitta reklamani jo'natish
+//	2.Mijozlar ro’yxatini pdf file ko'rinishida yuklab olish
+//User ---> Agar Tasdiqlash kodini to’g’ri kiritgan foydalanuvchi oddiy foydalanuvchi bo’lsa, unga quyidagi imkoniyatlar berilsin : 
+//        1.Berilgan nomi bo'yicha musiqa topib uni 10 ta lik pagination ko'rinishida olib kelib bersin
+//	2.Agar u youtube link bo'lsa youtubedan videoni yuklab bersin.
+//	3.Agar u intagram link bo'lsa instagramdan yuklab bersin.
+
+
+//Xasanboy Ishoqov => 5
+//Akramjon => 4
+//Muhammad Abdulloh => 4
+//Xushnudbek => 5
+//Xusan => 3
+//Tohirjon => 5
+//Robiya => 4
+//Maqsud => 3
+//Elyorbek => 5
+//Dilshodbek Abdullayev => 2
+//Hayotilla => 3
+//Ibrohim => 4
+//Nuriddin => 3
+//Abdulaziz => 1
+//Abduholiq => 3
+//Ozodbek => 2
+//Abdulloh => 4
+//Akmalho'ja => 2
+//Ismoil => 2
+//Asadulloh => 2
+//Umayr => 1
+//Usmon => 5
+//Nursulton => 1
+//Otabek => 1
+//Muhammad Sodiq => 1
