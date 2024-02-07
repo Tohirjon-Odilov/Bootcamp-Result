81157086556513e863b0f75a4b11d66dffe9bbea
Tohirjon-Odilov <shahanshoh819@gmail.com>
2024-02-07 13:13:50 +0500

    update files

---
+++ 3_month_linq_mongoDB_orm/42_lesson_mongoDB_intro/Program.cs
@@ -0,0 +1,23 @@
+﻿using Npgsql;
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Host=localhost;Port=5432;Database=PgConnect;User Id=postgres;Password=coder;";
+
+        using NpgsqlConnection connection = new NpgsqlConnection(connectionString);
+        connection.Open();
+        //var query = "insert into testtable (name) values ('Husniddin')";
+        var query = "select name from testtable";
+
+        using NpgsqlCommand cmd = new NpgsqlCommand(query, connection);
+
+        var result = cmd.ExecuteReader();
+        //Console.WriteLine(result);
+
+        while (result.Read())
+        {
+            Console.WriteLine(result[0]);
+        }
+    }
+}
