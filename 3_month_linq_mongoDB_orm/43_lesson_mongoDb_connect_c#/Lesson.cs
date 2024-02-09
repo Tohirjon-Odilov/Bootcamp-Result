ac22da8d7d93d84d110d51d81f4afe79f2ae772b
Tohirjon-Odilov <shahanshoh819@gmail.com>
2024-02-09 12:29:08 +0500

    update files

--- 3_month_linq_mongoDB_orm/43_lesson_mongoDb_connect_c#/Lesson.cs
+++ 3_month_linq_mongoDB_orm/43_lesson_mongoDb_connect_c#/Lesson.cs
@@ -1,7 +1,26 @@
-﻿namespace _43_lesson_mongoDb_connect_c_
+﻿using Npgsql;
+
+namespace _43_lesson_mongoDb_connect_c_
 {
     public class Lesson
     {
+        public Lesson()
+        {
+            const string CONNECTIONSTRING = "Server=127.0.0.1;Port=5432;Database=PgConnect;username=postgres;Password=coder;";
 
+            NpgsqlConnection connection = new NpgsqlConnection(CONNECTIONSTRING);
+
+            connection.Open();
+
+            string query = "create table if not exists users (id serial, name varchar(50), age int)";
+            NpgsqlCommand command = new NpgsqlCommand(query, connection);
+            Console.WriteLine(command.ExecuteNonQuery());
+
+            // logic
+            Console.Write("Database'ga muvaffaqiyatli ulandi!\n\n");
+
+
+            connection.Close();
+        }
     }
 }
--- 3_month_linq_mongoDB_orm/43_lesson_mongoDb_connect_c#/Program.cs
+++ 3_month_linq_mongoDB_orm/43_lesson_mongoDb_connect_c#/Program.cs
@@ -1,9 +1,11 @@
 ﻿using _43_lesson_mongoDb_connect_c_;
-using MongoDB.Bson;
-using MongoDB.Driver;
+//using MongoDB.Bson;
+//using MongoDB.Driver;
 
-var dbContext = new DbContext();
-IMongoDatabase database = dbContext.GetDatabaseByName("countyDb");
+//var dbContext = new DbContext();
+//IMongoDatabase database = dbContext.GetDatabaseByName("countyDb");
 
-List<BsonDocument>? collections = database.ListCollections().ToList();
-Console.WriteLine(collections);
+//List<BsonDocument>? collections = database.ListCollections().ToList();
+//Console.WriteLine(collections);
+
+var lesson = new Lesson();
