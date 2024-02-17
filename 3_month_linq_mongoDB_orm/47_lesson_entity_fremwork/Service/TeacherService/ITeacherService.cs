ac77ab2775a663d72337302fcc27d5b14abd0e40
Tohirjon-Odilov <shahanshoh819@gmail.com>
2024-02-17 11:53:13 +0500

    add service

---
+++ 3_month_linq_mongoDB_orm/47_lesson_entity_fremwork/Service/TeacherService/ITeacherService.cs
@@ -0,0 +1,13 @@
+﻿using _47_lesson_entity_fremwork.Models;
+
+namespace _47_lesson_entity_fremwork.Service.TeacherService
+{
+    public interface ITeacherService
+    {
+        public List<Teacher> GetAllTeacher();
+        public Teacher GetByIdTeacher(int id);
+        public string CreateTeacher(Teacher teacher);
+        public string UpdateTeacher(int id, Teacher teacher);
+        public string DeleteTeacher(int id);
+    }
+}
---
+++ 3_month_linq_mongoDB_orm/47_lesson_entity_fremwork/Service/TeacherService/TeacherService.cs
@@ -0,0 +1,32 @@
+﻿using _47_lesson_entity_fremwork.Models;
+
+namespace _47_lesson_entity_fremwork.Service.TeacherService
+{
+    public class TeacherService : ITeacherService
+    {
+        public string CreateTeacher(Teacher teacher)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string DeleteTeacher(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Teacher> GetAllTeacher()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Teacher GetByIdTeacher(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string UpdateTeacher(int id, Teacher teacher)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
