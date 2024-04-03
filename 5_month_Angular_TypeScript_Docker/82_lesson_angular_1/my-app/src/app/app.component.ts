86a07091662a8736d78d13ed74d2ed08a9913013
Tohirjon-Odilov <shahanshoh819@gmail.com>
2024-04-03 14:04:36 +0500

    add components

--- 5_month_Angular_TypeScript_Docker/82_lesson_angular_1/my-app/src/app/app.component.ts
+++ 5_month_Angular_TypeScript_Docker/82_lesson_angular_1/my-app/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- 5_month_Angular_TypeScript_Docker/82_lesson_angular_1/my-app/src/app/app.routes.ts
+++ 5_month_Angular_TypeScript_Docker/82_lesson_angular_1/my-app/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "home", title: "Home Page", component: HomeComponent},
+    {path: "**", title: "Home Page", component: FooterComponent},
+];
