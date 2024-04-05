550acae86b6557f34f912480b6562ca35772320b
Tohirjon-Odilov <shahanshoh819@gmail.com>
2024-04-05 09:39:22 +0500

    add some files

--- 5_month_Angular_TypeScript_Docker/83_lesson_angular_ng_bootstrap/MyLeetCode/src/app/app.component.ts
+++ 5_month_Angular_TypeScript_Docker/83_lesson_angular_ng_bootstrap/MyLeetCode/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./components/header/header.component";
+import {HomeComponent} from "./components/pages/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- 5_month_Angular_TypeScript_Docker/83_lesson_angular_ng_bootstrap/MyLeetCode/src/app/app.routes.ts
+++ 5_month_Angular_TypeScript_Docker/83_lesson_angular_ng_bootstrap/MyLeetCode/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./components/pages/home/home.component";
+import {NotfoundComponent} from "./components/pages/notfound/notfound.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "", title: "Home page", component: HomeComponent},
+  {path: "home", title: "Home page", component: HomeComponent},
+  {path: "**", title: "Not Found", component: NotfoundComponent},
+];
