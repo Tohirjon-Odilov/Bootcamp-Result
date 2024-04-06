d32318e9ac97f319227c3cf060bb600af515b43e
Tohirjon-Odilov <shahanshoh819@gmail.com>
2024-04-06 10:57:54 +0500

    complete homework

--- 5_month_Angular_TypeScript_Docker/83_lesson_angular_ng_bootstrap/MyLeetCode/src/app/app.routes.ts
+++ 5_month_Angular_TypeScript_Docker/83_lesson_angular_ng_bootstrap/MyLeetCode/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./components/pages/home/home.component";
 import {NotfoundComponent} from "./components/pages/notfound/notfound.component";
+import { InputComponent } from './components/pages/input/input.component';
 
 export const routes: Routes = [
   {path: "", title: "Home page", component: HomeComponent},
   {path: "home", title: "Home page", component: HomeComponent},
+  {path: "input", title: "Input page", component: InputComponent},
   {path: "**", title: "Not Found", component: NotfoundComponent},
 ];
--- 5_month_Angular_TypeScript_Docker/83_lesson_angular_ng_bootstrap/MyLeetCode/src/app/components/header/header.component.ts
+++ 5_month_Angular_TypeScript_Docker/83_lesson_angular_ng_bootstrap/MyLeetCode/src/app/components/header/header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  constructor(private route: ActivatedRoute, private router: Router) {}
 }
