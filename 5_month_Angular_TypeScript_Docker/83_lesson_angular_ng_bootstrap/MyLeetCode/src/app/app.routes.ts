60a5f947ddc2a194072b073c291e6b244b6dfdb2
Tohirjon-Odilov <shahanshoh819@gmail.com>
2024-04-14 12:42:31 +0500

    profile html complete

--- 5_month_Angular_TypeScript_Docker/83_lesson_angular_ng_bootstrap/MyLeetCode/src/app/app.routes.ts
+++ 5_month_Angular_TypeScript_Docker/83_lesson_angular_ng_bootstrap/MyLeetCode/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from "./components/pages/home/home.component";
 import {NotfoundComponent} from "./components/pages/notfound/notfound.component";
 import { InputComponent } from './components/pages/input/input.component';
+import { ProfileComponent } from './components/pages/profile/profile.component';
 
 export const routes: Routes = [
   {path: "", title: "Home page", component: HomeComponent},
   {path: "home", title: "Home page", component: HomeComponent},
   {path: "input", title: "Input page", component: InputComponent},
+  {path: "profile", title: "Profile page", component: ProfileComponent},
   {path: "**", title: "Not Found", component: NotfoundComponent},
 ];
