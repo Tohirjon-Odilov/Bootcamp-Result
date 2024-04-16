6684accbc93078c660505eee8e071d55806ff417
Tohirjon-Odilov <shahanshoh819@gmail.com>
2024-04-16 11:13:40 +0500

    delete files

--- 5_month_Angular_TypeScript_Docker/85_lesson_angular_with_api/materialUIApi/src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { GetAllComponent } from './components/get-all/get-all.component';
-
-export const routes: Routes = [
-    { path: "", component: GetAllComponent }
-];
