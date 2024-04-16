4f1d72b3a306ffba38b08eb66a9fdba9fba99159
Tohirjon-Odilov <shahanshoh819@gmail.com>
2024-04-16 06:45:50 +0500

    update files

---
+++ 5_month_Angular_TypeScript_Docker/85_lesson_angular_with_api/materiallUiWithApi/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { GetAllComponent } from './components/get-all/get-all.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    GetAllComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
