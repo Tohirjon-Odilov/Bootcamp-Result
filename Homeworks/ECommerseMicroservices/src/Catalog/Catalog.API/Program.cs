ff1ca2720c5cd94fc5636bb8f1b6e4210ffc61ee
tohirjon-odilov <shahanshoh819@gmail.com>
2024-05-10 09:02:59 +0500

    add files

--- Homeworks/ECommerseMicroservices/src/Catalog/Catalog.API/Program.cs
+++ Homeworks/ECommerseMicroservices/src/Catalog/Catalog.API/Program.cs
@@ -10,19 +10,14 @@ namespace Catalog.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
             builder.Services.AddCatalogApplicationDependencyInjection();
             builder.Services.AddCatalogInfrastructureDependencyInjection(builder.Configuration);
-            
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -30,12 +25,8 @@ namespace Catalog.API
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
-
             app.MapControllers();
-
             app.Run();
         }
     }
